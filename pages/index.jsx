46fadd92180ec068c82e4a90f93cb62f94084357
christaylor19 <christaylormail.94@gmail.com>
2021-08-03 17:27:17 +0100

    Add Supabase

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+import Account from '../components/Account';
+import Auth from '../components/Auth';
+import { supabase } from '../utils/supabase';
+
+export default function Home() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+    </div>
+  )
+}
